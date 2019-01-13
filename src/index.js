e419b58c6690a0a5b13e4cd22875d8ad33ceefef
Luigi <luigi@umbrellavalidator.com>
2019-01-12 23:46:14 -0800

    Show one resort

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import DarkSkyApi from 'dark-sky-api';
 DarkSkyApi.apiKey = "";
 
 let resortData = [
+  { name: "Snow Summit", zipcode: 92315, state: "CA", lat: "34.2380183", lon: "-116.8916176" },
   { name: "Mammoth", zipcode: 93546, state: "CA", lat: "37.6307674", lon: "-119.050186" },
   { name: "Squaw Valley", zipcode: 96146, state: "CA", lat: "39.1969822", lon: "-120.2431602" },
   { name: "Alpine Meadows", zipcode: 96146, state: "CA", lat: "39.1644547", lon: "-120.2408818" },
@@ -16,6 +17,7 @@ let resortData = [
 
 let lat = resortData[0].lat;
 let lon = resortData[0].lon;
+let resortName = resortData[0].name;
 
 // Dark Sky API: https://darksky.net/dev/docs
 //let endpoint = `https://api.darksky.net/forecast/${key}/${lat},${lon}?exclude=minutely,hourly`;
@@ -23,22 +25,27 @@ let lon = resortData[0].lon;
 //endpoint = `api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&APPID=${keyOpenWeather}`;
 
 let ResortWeatherResource = createResource(() => {
-  //let options = {
-  //  mode: "no-cors",
-  //  headers: {
-  //    //"Access-Control-Allow-Origin": "*",
-  //    "Content-Type": "text/plain",
-  //    //"Content-Type": "application/json",
-  //  }
-  //};
-  //fetch(endpoint, options).then(res => res.json())
-
   const position = {
     latitude: lat,
     longitude: lon,
   };
-  return DarkSkyApi.loadCurrent(position).then(res => console.log)
+  return DarkSkyApi.loadCurrent(position).then(res => {
+    console.log(res);
+    return res;
+  })
 });
+
+let ResortWeatherForecastResource = createResource(() => {
+  const position = {
+    latitude: lat,
+    longitude: lon,
+  };
+  return DarkSkyApi.loadForecast(position).then(res => {
+    console.log(res);
+    return res;
+  })
+});
+
 
 function ResortListItem({className, component: Component = "li", ...props}) {
   return (
@@ -59,14 +66,48 @@ function WeatherListItem({className, component: Component = "li", ...props}) {
 }
 
 function WeatherList(props) {
+  const dataWeather = ResortWeatherResource.read();
+  return (
+    <div>
+      <h3>{resortName}</h3>
+      Summary: {dataWeather.summary}<br />
+      precipIntensity: {dataWeather.precipIntensity}<br />
+      precipProbability: {dataWeather.precipProbability}<br />
+      temperature: {dataWeather.temperature}°F<br />
+    </div>
+  );
+}
+
+
+function WeatherList1(props) {
   return (
     <ul>
-      {ResortWeatherResource.read().daily.data.map(day => (
+      {ResortWeatherResource.read().daily.map(day => (
         <WeatherListItem key={day.time}>{day.summary}</WeatherListItem>
       ))}
     </ul>
   );
 }
+
+function WeatherForecastList(props) {
+  const dataWeatherForecast = ResortWeatherForecastResource.read();
+  const daily = [...dataWeatherForecast.daily.data]
+  console.log(daily)
+  return (
+    <div>
+      <h3>{resortName}</h3>
+      <ul>
+        {daily.map(day => (
+          <li key={day.time}>{new Date(day.dateTime) + ": " +
+            day.precipAccumulation + " (" +
+            day.precipProbability + ") " +
+            day.summary}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 
 function App() {
   return (
@@ -87,9 +128,13 @@ function App() {
       </ul>
       <h2>Weather</h2>
       <React.Suspense fallback={<div>...loading</div>}>
-        {/* <WeatherList /> */}
+        <WeatherList />
       </React.Suspense>
-    </div>
+      <h2>Forecast</h2>
+       <React.Suspense fallback={<div>...loading</div>}>
+        <WeatherForecastList />
+      </React.Suspense>
+   </div>
   );
 }
 
